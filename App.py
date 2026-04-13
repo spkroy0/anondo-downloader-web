23d86658c671611f910a0539ac119d5aefc9326a
Sree Anondo Kumar Roy <spkroy2006@gmail.com>
2026-04-13 09:25:06 +0600

    Add C editor route to serve c_editor.html
    Added a new route for the C editor that serves c_editor.html.

--- App.py
+++ App.py
@@ -11,6 +11,15 @@ def index():
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     return response
 
+# --- C Editor এর জন্য নতুন রুট ---
+@app.route('/c-editor')
+def c_editor():
+    # এটি templates ফোল্ডার থেকে c_editor.html ফাইলটি লোড করবে
+    response = make_response(render_template('c_editor.html'))
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    # প্রোডাকশন এনভায়রনমেন্টে debug=False রাখা ভালো
     app.run(host='0.0.0.0', port=port, debug=False)
